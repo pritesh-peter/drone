59937ad07b97d0b0064cff94040081b7f2da5c8d
pritesh-peter <pritesh.peter@gmail.com>
2023-04-27 00:01:41 +0545

    initial commit

---
+++ src/test/java/com/project/drone/DroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
