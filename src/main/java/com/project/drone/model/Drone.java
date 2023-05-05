bcbbe6189c26c280d447bcf8447b72c8e44c0fdf
pritesh-peter <pritesh.peter@gmail.com>
2023-05-05 19:19:25 +0545

    created Drone model

---
+++ src/main/java/com/project/drone/model/Drone.java
@@ -0,0 +1,13 @@
+package com.project.drone.model;
+
+public class Drone {
+    String serialNumber;
+
+    String model;
+
+    Float loadWeight;
+
+    Float batteryCapacity;
+
+    DroneState droneState;
+}
