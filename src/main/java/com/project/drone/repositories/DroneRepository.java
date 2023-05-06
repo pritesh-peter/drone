5bc66fe3d6ec83ae63d60e794bb0056597a0c61f
pritesh-peter <pritesh.peter@gmail.com>
2023-05-06 16:43:28 +0545

    created Drone Repository

---
+++ src/main/java/com/project/drone/repositories/DroneRepository.java
@@ -0,0 +1,9 @@
+package com.project.drone.repositories;
+
+import com.project.drone.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneRepository extends JpaRepository<Drone,String> {
+
+    
+}
